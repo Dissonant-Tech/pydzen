d21adbd9be9762cbeb9bb2de7094e296ae4b2633
David Rodriguez <dissonant.tech@gmail.com>
2015-01-08 13:17:45 -0500

    Added TitlePlugin, replaced plugins.title

--- plugins/titlePlugin.py
+++ plugins/titlePlugin.py
@@ -4,3 +4,24 @@ import subprocess
 
 from pydzen import config
 from plugin import Plugin, Mode, Position
+
+class TitlePlugin(Plugin):
+    """Prints title of current window"""
+    def __init__(self):
+        super(TitlePlugin, self).__init__()
+
+    def update(self, queue):
+        try:
+            sub = subprocess.Popen(['xtitle', '-s'], stdout=subprocess.PIPE)
+            while True:
+                line = sub.stdout.readline()
+                
+                # FIXME for some reason decoding the bytes makes line = None
+                # but straight conversion to string leaves b'title...\n'
+                line = str(line)
+                line = line[2:-3]
+
+                title = (line[:45]+'...') if len(line) > 45 else line
+                queue.put({self.__class__.__name__: title})
+        except Exception as e:
+            self._logger.exception(e)
